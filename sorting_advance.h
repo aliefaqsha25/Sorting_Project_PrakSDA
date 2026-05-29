4ee7bdf4cbdb8bcfb96f34ee935dd37d92b4b37a
aliefaqsha25 <aliefaqsha77@gmail.com>
2026-05-29 17:19:51 +0700

    Add sorting functions header file

---
+++ sorting_advance.h
@@ -0,0 +1,8 @@
+#ifndef SORTING_ADVANCE_H
+#define SORTING_ADVANCE_H
+
+void shell_sort(char **arr, int n);
+void quick_sort(char **arr, int low, int high);
+void merge_sort(char **arr, int left, int right);
+
+#endif
